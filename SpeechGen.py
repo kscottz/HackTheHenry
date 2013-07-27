518f62bbe69c38fc0ae0eeb5d2892837bf62d63c
kscottz <kat@sightmachine.com>
2013-07-27 11:39:30 -0400

    got transcoding done

--- SpeechGen.py
+++ SpeechGen.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import urllib
-
+import subprocess
+import os
 
 def getObjData(id='83.190.1'):    
     itemstr = 'http://api.makerfairedetroit.com/item.aspx?objectid={0}'.format(id)
@@ -13,7 +14,27 @@ def getObjData(id='83.190.1'):
             retVal[k] = v
     return retVal
 
+def textToWav(text,file_name):
+   subprocess.call(["espeak", "-s90","-a150", "-w"+file_name,text])
+
+def wavToMp3(infile='temp.wav',outfile='temp.mp3'):
+    os.system('lame -s22.05k -b16k {0} {1}'.format( infile, outfile))
+
+
 stuff = getObjData()
-print 30*'$'
-for key, value in stuff.iteritems():
-    print "{0}-->{1}".format(key,value)
+
+textToWav(stuff['abstract'],'temp.wav')
+wavToMp3()
+
+# print 30*'$'
+# for key, value in stuff.iteritems():
+#     print "{0}-->{1}".format(key,value)
+
+
+
+# import pyttsx
+# engine = pyttsx.init()
+# engine.setProperty('rate',120)
+# engine.setProperty('volume',1)
+# engine.say(stuff['abstract'])
+# engine.runAndWait()
